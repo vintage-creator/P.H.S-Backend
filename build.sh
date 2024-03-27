28b1b3f55eb542d3494d8004473ea5a8cb681105
Israel Abazie <you@example.com>
2024-03-27 05:05:04 +0100

    Add Gunicorn WSGI server

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
+
+# Stop execution if any command fails
 set -o errexit
 
-pip install -r requirements.txt
+# Navigate into the directory where manage.py is located
+cd handyman_project
 
+# Install dependencies
+pip install -r ../requirements.txt
+
+# Apply migrations
 python manage.py migrate
--- handyman_project/settings.py
+++ handyman_project/settings.py
@@ -48,7 +48,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = ["localhost", "phs.onrender.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "phs.onrender.com"]
 
 
 # Application definition
