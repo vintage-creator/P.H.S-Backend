3807535761c1f24c92266ad6321eca3ced9e57ca
Israel Abazie <you@example.com>
2024-03-27 05:57:08 +0100

    modify build.sh

--- build.sh
+++ build.sh
@@ -3,11 +3,8 @@
 # Stop execution if any command fails
 set -o errexit
 
-# Navigate into the directory where manage.py is located
-cd handyman_project
-
 # Install dependencies
-pip install -r ../requirements.txt
+pip install -r requirements.txt
 
 # Apply migrations
-python manage.py migrate
+python handyman_project/manage.py migrate
