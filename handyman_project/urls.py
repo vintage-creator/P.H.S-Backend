770e7ccc14a02d5018561120340f5b7a04871053
Israel Abazie <you@example.com>
2024-03-27 08:05:49 +0100

    shift dirs

--- handyman_project/urls.py
+++ handyman_project/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.views.generic import TemplateView
 from django.urls import path, include
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='api_documentation.html'), name='api-docs'),
     path('admin/', admin.site.urls),
     path('', include('handyman.urls')),
     path('auth/', include('djoser.urls')), #Signup endpoint
